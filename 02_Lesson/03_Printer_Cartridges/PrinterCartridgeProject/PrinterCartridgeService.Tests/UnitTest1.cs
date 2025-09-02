f6feaac50c02687bedc5224e47801bb2b1165dfe
Brezzor <oliver.bresson@gmail.com>
2025-09-02 20:41:52 +0200

    Initial printer cartridge test setup

---
+++ 02_Lesson/03_Printer_Cartridges/PrinterCartridgeProject/PrinterCartridgeService.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace PrinterCartridgeService.Tests;
+
+[TestFixture]
+public class PrinterCartridgeServiceTests
+{
+    private PrinterCartridgeDiscount _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new PrinterCartridgeDiscount();
+    }
+
+    [TestCase(100, 0.2)]
+    public void Test1(int amount, double expectedDiscount)
+    {
+        Assert.That(_service.CalculateDiscount(amount), Is.EqualTo(expectedDiscount));
+    }
+}
---
+++ 02_Lesson/03_Printer_Cartridges/PrinterCartridgeProject/PrinterCartridgeService/PrinterCartridgeDiscount.cs
@@ -0,0 +1,17 @@
+﻿namespace PrinterCartridgeService;
+
+public class PrinterCartridgeDiscount
+{
+    public double CalculateDiscount(int cartridgeCount)
+    {
+        if (cartridgeCount < 5)
+        {
+            throw new ArgumentException("The minimum order quantity is 5.");
+        }
+        else if (cartridgeCount >= 100)
+        {
+            return 0.2;
+        }
+        return 0;
+    }
+}
