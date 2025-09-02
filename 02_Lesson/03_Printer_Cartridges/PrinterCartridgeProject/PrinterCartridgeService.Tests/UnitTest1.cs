b5de44a1fe0ae4d56ad6d6b5c0f2d3de8e66e84e
Brezzor <oliver.bresson@gmail.com>
2025-09-02 21:00:49 +0200

    Update to printer cartridge tests

--- 02_Lesson/03_Printer_Cartridges/PrinterCartridgeProject/PrinterCartridgeService.Tests/UnitTest1.cs
+++ 02_Lesson/03_Printer_Cartridges/PrinterCartridgeProject/PrinterCartridgeService.Tests/UnitTest1.cs
@@ -1,7 +1,4 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Legacy;
-
-namespace PrinterCartridgeService.Tests;
+﻿namespace PrinterCartridgeService.Tests;
 
 [TestFixture]
 public class PrinterCartridgeServiceTests
@@ -14,9 +11,32 @@ public class PrinterCartridgeServiceTests
         _service = new PrinterCartridgeDiscount();
     }
 
+    [TestCase(5, 0)]
+    [TestCase(6, 0)]
+    [TestCase(47, 0)]
+    [TestCase(98, 0)]
+    [TestCase(99, 0)]
     [TestCase(100, 0.2)]
+    [TestCase(101, 0.2)]
+    [TestCase(167, 0.2)]
     public void Test1(int amount, double expectedDiscount)
     {
         Assert.That(_service.CalculateDiscount(amount), Is.EqualTo(expectedDiscount));
+    }
+
+    [TestCase(-15)]
+    [TestCase(-2)]
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(4)]
+    [TestCase(-10)]
+    [TestCase(-167)]
+    public void Test2(int amount)
+    {
+        var ex = Assert.Throws<ArgumentException>(() => _service.CalculateDiscount(amount));
+        Assert.That(ex.Message, Is.EqualTo("The minimum order quantity is 5."));
     }
 }
