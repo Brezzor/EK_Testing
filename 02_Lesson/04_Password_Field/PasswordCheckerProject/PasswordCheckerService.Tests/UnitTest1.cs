bb047abfc30858a955da46853c183b94a2e93eb9
Oliver Bresson <ob@beezy365.com>
2025-09-03 09:41:40 +0200

    Initial password checker exercise

---
+++ 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace PasswordCheckerService.Tests;
+
+[TestFixture]
+public class PasswordCheckerServiceTests
+{
+    private PasswordChecker _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new PasswordChecker();
+    }
+
+    [TestCase("")]
+    public void Test1(string password)
+    {
+        Assert.Throws<ArgumentException>(() => _service.CheckPassword(password));
+    }
+}
---
+++ 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService/PasswordChecker.cs
@@ -0,0 +1,12 @@
+﻿namespace PasswordCheckerService;
+
+public class PasswordChecker
+{
+    public void CheckPassword(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentException("Password cannot be null or empty.");
+        }
+    }
+}
