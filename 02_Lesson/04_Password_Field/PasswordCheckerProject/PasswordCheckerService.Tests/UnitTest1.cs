db9768a082707616873efd02093bf0e73e7d09ad
Oliver Bresson <ob@beezy365.com>
2025-09-08 13:04:15 +0200

    Finished password checker test

--- 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService.Tests/UnitTest1.cs
+++ 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService.Tests/UnitTest1.cs
@@ -12,8 +12,25 @@ public class PasswordCheckerServiceTests
     }
 
     [TestCase("")]
-    public void Test1(string password)
+    [TestCase("P")]
+    [TestCase("Pa")]
+    [TestCase("Pas")]
+    [TestCase("Pass")]
+    [TestCase("Password123")]
+    [TestCase("Password1234")]
+    [TestCase("Password1234567890")]
+    public void Negative_Test(string password)
     {
         Assert.Throws<ArgumentException>(() => _service.CheckPassword(password));
+    }
+
+    [TestCase("Passw")]
+    [TestCase("Passwo")]
+    [TestCase("Password")]
+    [TestCase("Password1")]
+    [TestCase("Password12")]
+    public void Positive_Test(string password)
+    {
+        Assert.DoesNotThrow(() => _service.CheckPassword(password));
     }
 }
--- 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService/PasswordChecker.cs
+++ 02_Lesson/04_Password_Field/PasswordCheckerProject/PasswordCheckerService/PasswordChecker.cs
@@ -8,5 +8,13 @@ public class PasswordChecker
         {
             throw new ArgumentException("Password cannot be null or empty.");
         }
+        else if (password.Length < 5)
+        {
+            throw new ArgumentException("Password must be at least 5 characters long.");
+        }
+        else if (password.Length > 10)
+        {
+            throw new ArgumentException("Password cannot be more than 10 characters long.");
+        }
     }
 }
