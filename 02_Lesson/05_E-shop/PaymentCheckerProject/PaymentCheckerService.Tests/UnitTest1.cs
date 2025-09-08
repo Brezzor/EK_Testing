977a13a4d74ad268d7657370496b19f6a8a0002c
Brezzor <oliver.bresson@gmail.com>
2025-09-08 22:16:41 +0200

    Begun E-shop test exercise

---
+++ 02_Lesson/05_E-shop/PaymentCheckerProject/PaymentCheckerService.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace PaymentCheckerService.Tests;
+
+public class Tests
+{
+    private PaymentChecker _paymentChecker;
+
+    [SetUp]
+    public void Setup()
+    {
+        _paymentChecker = new PaymentChecker();
+    }
+
+    [TestCase(0.00, typeof(ArgumentOutOfRangeException))]
+    public void Test1(decimal amount, Type expectedException)
+    {
+        Assert.Throws(expectedException, () => _paymentChecker.CheckPayment(amount));
+    }
+
+    [TestCase(0.01, 0.01)]
+    [TestCase(0.02, 0.02)]
+    [TestCase(151.00, 151.00)]
+    [TestCase(299.99, 299.99)]
+    [TestCase(300.00, 300.00)]
+    [TestCase(300.01, 285.01)]
+    [TestCase(300.02, 285.02)]
+    [TestCase(550.05, 522.55)]
+    [TestCase(799.99, 759.99)]
+    [TestCase(800.00, 760.00)]
+    [TestCase(800.01, 720.01)]
+    [TestCase(800.02, 720.02)]
+    [TestCase(1500.00, 1350.00)]
+    public void Test2(decimal amount, decimal expectedResult)
+    {
+        Assert.That(_paymentChecker.CheckPayment(amount), Is.EqualTo(expectedResult));
+    }
+}
---
+++ 02_Lesson/05_E-shop/PaymentCheckerProject/PaymentCheckerService/PaymentChecker.cs
@@ -0,0 +1,26 @@
+﻿namespace PaymentCheckerService;
+
+public class PaymentChecker
+{
+    public decimal CheckPayment(decimal amount)
+    {
+        decimal _amount = decimal.Round(amount, 2);
+
+        if (_amount < 0.01m)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be larger than 0.01.");
+        }
+        else if (_amount <= 300.00m)
+        {
+            return _amount;
+        }
+        else if (_amount <= 800.00m)
+        {
+            return decimal.Round(_amount * 0.95m, 2);
+        }
+        else
+        {
+            return decimal.Round(_amount * 0.9m, 2);
+        }
+    }
+}
