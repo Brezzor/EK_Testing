ba64efb3afb38642af8c6336ec656e2384c7ae7a
Oliver Bresson <ob@beezy365.com>
2025-09-09 10:31:31 +0200

    Test setup for using Moq with NUnit.

---
+++ 03_Lesson/Test_Framework_Mock_Test/MockTestProject/MockTest.Tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+
+namespace MockTest.Tests;
+
+[TestFixture]
+public class Tests
+{
+    private Mock<ICusomerRepository> _mockCustomerRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockCustomerRepository = new Mock<ICusomerRepository>();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        var customerId = 1;
+        var expectedCustomer = new Customer
+        {
+            Id = customerId,
+            Name = "John Doe",
+            Email = "John.Doe@Test.com",
+            PhoneNumber = "123-456-7890"
+        };
+
+        _mockCustomerRepository
+            .Setup(repo => repo.GetCustomerById(customerId))
+            .Returns(expectedCustomer);
+
+        var customerService = new CustomerService(_mockCustomerRepository.Object);
+
+        // Act
+        var actualCustomer = customerService.GetCustomer(customerId);
+
+        // Assert
+        Assert.That(actualCustomer, Is.Not.Null);
+        Assert.That(expectedCustomer.Id, Is.EqualTo(actualCustomer.Id));
+        Assert.That(expectedCustomer.Name, Is.EqualTo(actualCustomer.Name));
+        Assert.That(expectedCustomer.Email, Is.EqualTo(actualCustomer.Email));
+        Assert.That(expectedCustomer.PhoneNumber, Is.EqualTo(actualCustomer.PhoneNumber));
+
+        _mockCustomerRepository
+            .Verify(repo => repo.GetCustomerById(customerId), Times.Once, "GetCustomerById was not called exactly once.");
+    }
+}
---
+++ 03_Lesson/Test_Framework_Mock_Test/MockTestProject/MockTest/TestLib.cs
@@ -0,0 +1,29 @@
+﻿namespace MockTest;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
+
+public interface ICusomerRepository
+{
+    public Customer GetCustomerById(int id);
+}
+
+public class CustomerService
+{
+    private readonly ICusomerRepository _customerRepository;
+
+    public CustomerService(ICusomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public Customer GetCustomer(int id)
+    {
+        return _customerRepository.GetCustomerById(id);
+    }
+}
